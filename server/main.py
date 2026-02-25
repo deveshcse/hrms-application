eda6a2f535e5ba4f7b12abd2ff62ca3c39549c86
deveshcse <d.mishra56603@gmail.com>
2026-02-25 18:07:34 +0530

    fix: cors middleware

--- server/main.py
+++ server/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 from core.config import settings
 from core.exceptions import AppException
 from core.response import error_response
@@ -9,9 +11,17 @@ from modules.attendance.routes import router as attendance_router
 
 app = FastAPI(title=settings.app_name)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(employee_router)
 app.include_router(attendance_router)
+
 
 @app.exception_handler(AppException)
 async def app_exception_handler(request: Request, exc: AppException):
